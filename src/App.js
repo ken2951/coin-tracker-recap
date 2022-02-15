474e180087f870df809486fef53b7b0dd5a3a78d
Gihyeon KIM <ken29511@gmail.com>
2022-02-15 23:52:14 +0900

    recap: useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(coins);
+  return (
+    <div>
+      <h1>Cryptocurrency Converter Calculator</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol})
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
 }
 
 export default App;
